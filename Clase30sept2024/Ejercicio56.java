2ec8dd05bd26a95739f818df4d8b51848ca39652
JhonHCanoB <jhon.canob@campusucc.edu.co>
2024-09-30 20:36:27 +0000

    Finalizacion ejercicio 56

--- Clase30sept2024/Ejercicio56.java
+++ Clase30sept2024/Ejercicio56.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 public class Ejercicio56 {
     public static void main(String[] args){
+        double nota = 0.0;
+        int aprobados = 0, noAprobados = 0;
+        //notas de los estudiantes
+        Scanner leer = new Scanner(System.in);
+        System.out.print("Digite la cantidad de estudiantes: ");
+        int estudiantes = leer.nextInt();
 
-        System.out.println("Mi primer programa de JAVA en Github");
-        
+        for(int i = 1; i <= estudiantes; i++){
+            System.out.print("Nota del estudiante " + i + ": ");
+            nota = leer.nextDouble();
+
+            if (nota >= 5.0){
+                aprobados++;
+            }else{
+                noAprobados++;
+            }
+        }
+
+        System.out.println("Aprobados: " + aprobados + " No Aprobados: " + noAprobados);
+
     }
 }
