d5f9f202738aac0d8f0d08f6199f045274b94930
JhonHCanoB <jhon.canob@campusucc.edu.co>
2025-02-17 20:02:37 +0000

    grados

---
+++ 12025Uniajc/Clase3/Convertidor.java
@@ -0,0 +1,11 @@
+public class Convertidor {
+    public static void main(String[] args) {
+        //Declaración de las variables para el ejercicio
+        // c -> grados celsius, f -> grados farenheit
+        // gradosCelcius, gradosFarenheit 
+        double c, f, gradosCelcius, gradosFarenheit;
+
+        
+
+    }
+}
