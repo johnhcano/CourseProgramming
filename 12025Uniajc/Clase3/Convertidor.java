ca109f3ad4a03f61e709b7a977efefc381e71e9e
JhonHCanoB <jhon.canob@campusucc.edu.co>
2025-02-17 20:33:46 +0000

    Pidiendo datos por teclado

--- 12025Uniajc/Clase3/Convertidor.java
+++ 12025Uniajc/Clase3/Convertidor.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Convertidor {
     public static void main(String[] args) {
         //Declaración de las variables para el ejercicio
@@ -5,7 +7,14 @@ public class Convertidor {
         // gradosCelcius, gradosFarenheit 
         double c, f, gradosCelcius, gradosFarenheit;
 
-        
+        //Pedir datos por teclado
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Digite los grados Celcius");
+        c = leer.nextInt();
+        System.out.println("Digite lo grados Farenheit");
+        f = leer.nextInt();
 
+
+        leer.close();
     }
 }
