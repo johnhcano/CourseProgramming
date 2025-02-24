4c256a2b5adf60ebc2412af37682dcd964888c89
JhonHCanoB <jhon.canob@campusucc.edu.co>
2025-02-24 17:30:26 +0000

    Organizado

--- 20251/Ejercicios/Ejercicio56.java
+++ 20251/Ejercicios/Ejercicio56.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
 
 public class Ejercicio56 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         double nota = 0.0;
         int aprobados = 0, noAprobados = 0;
-        //notas de los estudiantes
+        // notas de los estudiantes
         Scanner leer = new Scanner(System.in);
         System.out.print("Digite la cantidad de estudiantes: ");
         int estudiantes = leer.nextInt();
 
-        for(int i = 1; i <= estudiantes; i++){
+        for (int i = 1; i <= estudiantes; i++) {
             System.out.print("Nota del estudiante " + i + ": ");
             nota = leer.nextDouble();
 
-            if (nota >= 5.0){
+            if (nota >= 5.0) {
                 aprobados++;
-            }else{
+            } else {
                 noAprobados++;
             }
         }
