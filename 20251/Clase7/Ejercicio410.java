6bb7ce28616448902db7ce592141d233687d39e3
JhonHCanoB <jhon.canob@campusucc.edu.co>
2025-03-31 19:19:52 +0000

    Creación del ejercicio 4 punto 10

---
+++ 20251/Clase7/Ejercicio410.java
@@ -0,0 +1,37 @@
+/*
+ * Pseudocodigo
+ * Algoritmo Sistema_de_Calificaciones
+ * var
+ *  entero: nota
+ * inicio
+ *  mostrar "Digite la calificación:"
+ *  leer nota
+ *  si (nota < 0 || nota > 100) entonces
+ *      mostrar "Número fuera de rango"
+ *  si_no
+ *      si (nota >= 90 && nota <= 100) entonces
+ *          mostrar "A"
+ *      si_no
+ *          si (nota >= 80 && nota < 90) entonces
+ *              mostrar "B"
+ *          si_no
+ *              si (nota >= 70 && nota < 80) entonces
+ *                  mostrar "C"
+ *              si_no
+ *                  si (nota >= 69 && nota < 70) entonces
+ *                      mostrar "D"
+ *                  si_no
+ *                      mostrar "F"
+ *                  fin_si
+ *               fin_si
+ *          fin_si
+ *      fin_si
+ *  fin_si
+ */
+
+public class Ejercicio410 {
+    public static void main(String[] args){
+
+        
+    }
+}
