edaebd5dac905982fa431055e65d786eead507bd
JhonHCanoB <jhon.canob@campusucc.edu.co>
2025-04-21 20:46:10 +0000

    update

--- 20251/Clase11_POO/Circulo.java
+++ 20251/Clase11_POO/Circulo.java
@@ -1,21 +1,18 @@
 public class Circulo {
-    
+   
     //Atributos
     private double radio;
-
+ 
     //Constructor de la clase: nos permite inicializar la clase
     //El método constructor se llama igual que la clase
-    
+   
     public Circulo(double radio){
         this.radio = radio; //this es una autoreferencia al atributo de la clase
     }
-
+ 
     //Métodos
     public double calcularPerimetro(){
         return 2 * Math.PI * radio;
     }
-
-    public double calcularArea(){
-        return Math.PI * (radio * radio); // Math.PI * Math.pow(radio,2)
-    }
 }
+ 
--- 20251/Clase11_POO/EjecutarCirculo.java
+++ 20251/Clase11_POO/EjecutarCirculo.java
@@ -1,15 +1,11 @@
 public class EjecutarCirculo{
     public static void main(String[] args){
+ 
+        double r = 2.3;
 
-        double r = 2.3; //radio
-        
-        //Creación del objeto
-        Circulo objCirculo1 = new Circulo(r);
+        Circulo objC1 = new Circulo(r);
 
-        //llamado a los métodos de la clase
-        System.out.println("Perímetro: " + objCirculo1.calcularPerimetro());
-
-        System.out.println("Área: " + objCirculo1.calcularArea());
 
     }
 }
+ 
