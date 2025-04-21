393a056e312862064157c7927a3048835222b62e
JhonHCanoB <jhon.canob@campusucc.edu.co>
2025-04-21 20:54:26 +0000

    update

---
+++ 20251/ClasePOO/EjecutarCirculo.java
@@ -0,0 +1,16 @@
+public class EjecutarCirculo{
+    public static void main(String[] args){
+ 
+        double r = 2.3; //radio
+       
+        //Creación del objeto
+        Circulo objCirculo1 = new Circulo(r);
+ 
+        //llamado a los métodos de la clase
+        System.out.println("Perímetro: " + objCirculo1.calcularPerimetro());
+ 
+        System.out.println("Área: " + objCirculo1.calcularArea());
+ 
+    }
+}
+ 
