74842444498e459dd07138c32bf115c10eb66273
JhonHCanoB <jhon.canob@campusucc.edu.co>
2025-09-15 19:56:07 +0000

    CuatroPuntoTrece

--- 20252/Clase7/CuatroPuntoTrece.java
+++ 20252/Clase7/CuatroPuntoTrece.java
@@ -23,26 +23,81 @@ public class CuatroPuntoTrece {
                     }
                     break;
             case 2: System.out.println("FEBRERO");
+                    if (dia >= 1 && dia <= 28){
+                        System.out.println("Día correcto");
+                    }else{
+                        System.out.println("Día incorrecto");
+                    }
                     break;
             case 3: System.out.println("MARZO");
+                    if (dia >= 1 && dia <= 31){
+                        System.out.println("Día correcto");
+                    }else{
+                        System.out.println("Día incorrecto");
+                    }
                     break;
             case 4: System.out.println("ABRIL");
+                    if (dia >= 1 && dia <= 30){
+                        System.out.println("Día correcto");
+                    }else{
+                        System.out.println("Día incorrecto");
+                    }
                     break;
             case 5: System.out.println("MAYO");
+                    if (dia >= 1 && dia <= 31){
+                        System.out.println("Día correcto");
+                    }else{
+                        System.out.println("Día incorrecto");
+                    }
                     break;
             case 6: System.out.println("JUNIO");
+                    if (dia >= 1 && dia <= 30){
+                        System.out.println("Día correcto");
+                    }else{
+                        System.out.println("Día incorrecto");
+                    }
                     break;
             case 7: System.out.println("JULIO");
+                    if (dia >= 1 && dia <= 31){
+                        System.out.println("Día correcto");
+                    }else{
+                        System.out.println("Día incorrecto");
+                    }
                     break;
             case 8: System.out.println("AGOSTO");
+                    if (dia >= 1 && dia <= 31){
+                        System.out.println("Día correcto");
+                    }else{
+                        System.out.println("Día incorrecto");
+                    }
                     break;
             case 9: System.out.println("SEPTIEMBRE");
+                    if (dia >= 1 && dia <= 30){
+                        System.out.println("Día correcto");
+                    }else{
+                        System.out.println("Día incorrecto");
+                    }
                     break;
             case 10: System.out.println("OCTUBRE");
+                    if (dia >= 1 && dia <= 31){
+                        System.out.println("Día correcto");
+                    }else{
+                        System.out.println("Día incorrecto");
+                    }
                     break;
             case 11: System.out.println("NOVIEMBRE");
+                    if (dia >= 1 && dia <= 30){
+                        System.out.println("Día correcto");
+                    }else{
+                        System.out.println("Día incorrecto");
+                    }
                     break;
             case 12: System.out.println("DICIEMBRE");
+                    if (dia >= 1 && dia <= 31){
+                        System.out.println("Día correcto");
+                    }else{
+                        System.out.println("Día incorrecto");
+                    }
                     break;
             default: System.out.println("Número fuera del rango ente 1 y 12");
         }
