94764b48e23aec64011ede48e8a1fbb04abc6474
JhonHCanoB <jhon.canob@campusucc.edu.co>
2025-09-15 20:09:03 +0000

    CuatroPuntoTrece

--- 20252/Clase7/CuatroPuntoTrece.java
+++ 20252/Clase7/CuatroPuntoTrece.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class CuatroPuntoTrece {
     public static void main(String[] args) {
         
+        //Declarar una constante
+        String MSG_OK_DIA = "El número introducido es válido como día";
+        String MSG_ERROR_DIA = "El número introducido NO es válido como día";
+
         //La clase Scanner permite leer datos por teclado
         Scanner leer = new Scanner(System.in);
 
@@ -17,89 +21,89 @@ public class CuatroPuntoTrece {
         switch(mes){
             case 1: System.out.println("ENERO");
                     if (dia >= 1 && dia <= 31){
-                        System.out.println("Día correcto");
+                        System.out.println(MSG_OK_DIA);
                     }else{
-                        System.out.println("Día incorrecto");
+                        System.out.println(MSG_ERROR_DIA);
                     }
                     break;
             case 2: System.out.println("FEBRERO");
-                    if (dia >= 1 && dia <= 28){
-                        System.out.println("Día correcto");
+                    if (dia >= 1 && dia <= 29){
+                        System.out.println(MSG_OK_DIA);
                     }else{
-                        System.out.println("Día incorrecto");
+                        System.out.println(MSG_ERROR_DIA);
                     }
                     break;
             case 3: System.out.println("MARZO");
                     if (dia >= 1 && dia <= 31){
-                        System.out.println("Día correcto");
+                        System.out.println(MSG_OK_DIA);
                     }else{
-                        System.out.println("Día incorrecto");
+                        System.out.println(MSG_ERROR_DIA);
                     }
                     break;
             case 4: System.out.println("ABRIL");
                     if (dia >= 1 && dia <= 30){
-                        System.out.println("Día correcto");
+                        System.out.println(MSG_OK_DIA);
                     }else{
-                        System.out.println("Día incorrecto");
+                        System.out.println(MSG_ERROR_DIA);
                     }
                     break;
             case 5: System.out.println("MAYO");
                     if (dia >= 1 && dia <= 31){
-                        System.out.println("Día correcto");
+                        System.out.println(MSG_OK_DIA);
                     }else{
-                        System.out.println("Día incorrecto");
+                        System.out.println(MSG_ERROR_DIA);
                     }
                     break;
             case 6: System.out.println("JUNIO");
                     if (dia >= 1 && dia <= 30){
-                        System.out.println("Día correcto");
+                        System.out.println(MSG_OK_DIA);
                     }else{
-                        System.out.println("Día incorrecto");
+                        System.out.println(MSG_ERROR_DIA);
                     }
                     break;
             case 7: System.out.println("JULIO");
                     if (dia >= 1 && dia <= 31){
-                        System.out.println("Día correcto");
+                        System.out.println(MSG_OK_DIA);
                     }else{
-                        System.out.println("Día incorrecto");
+                        System.out.println(MSG_ERROR_DIA);
                     }
                     break;
             case 8: System.out.println("AGOSTO");
                     if (dia >= 1 && dia <= 31){
-                        System.out.println("Día correcto");
+                        System.out.println(MSG_OK_DIA);
                     }else{
-                        System.out.println("Día incorrecto");
+                        System.out.println(MSG_ERROR_DIA);
                     }
                     break;
             case 9: System.out.println("SEPTIEMBRE");
                     if (dia >= 1 && dia <= 30){
-                        System.out.println("Día correcto");
+                        System.out.println(MSG_OK_DIA);
                     }else{
-                        System.out.println("Día incorrecto");
+                        System.out.println(MSG_ERROR_DIA);
                     }
                     break;
             case 10: System.out.println("OCTUBRE");
                     if (dia >= 1 && dia <= 31){
-                        System.out.println("Día correcto");
+                        System.out.println(MSG_OK_DIA);
                     }else{
-                        System.out.println("Día incorrecto");
+                        System.out.println(MSG_ERROR_DIA);
                     }
                     break;
             case 11: System.out.println("NOVIEMBRE");
                     if (dia >= 1 && dia <= 30){
-                        System.out.println("Día correcto");
+                        System.out.println(MSG_OK_DIA);
                     }else{
-                        System.out.println("Día incorrecto");
+                        System.out.println(MSG_ERROR_DIA);
                     }
                     break;
             case 12: System.out.println("DICIEMBRE");
                     if (dia >= 1 && dia <= 31){
-                        System.out.println("Día correcto");
+                        System.out.println(MSG_OK_DIA);
                     }else{
-                        System.out.println("Día incorrecto");
+                        System.out.println(MSG_ERROR_DIA);
                     }
                     break;
-            default: System.out.println("Número fuera del rango ente 1 y 12");
+            default: System.out.println("El número introducido no es válido como mes");
         }
     //Se debe cerrar el objeto de la clase Scanner
     leer.close();
