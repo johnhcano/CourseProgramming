332314115f51445c018d0e2fb94de7d1e1e5dac3
JhonHCanoB <jhon.canob@campusucc.edu.co>
2025-09-29 21:26:32 +0000

    captura de datos por teclado

---
+++ 20252/Clase9/CapturaDatos.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class CapturaDatos {
+    public static void main(String[] args) {
+        
+        Scanner leer = new Scanner(System.in);
+
+        //Ejemplo 1 - Cadenas
+        System.out.println("Digite nombre...");
+        String name = leer.next();
+        System.out.println("EL valor digitado es: " + name);
+
+        //Ejemplo 2 - Números enteros
+        System.out.println("Digite un número entero...");
+        int numero = leer.nextInt();
+        System.out.println("EL valor digitado es: " + numero);
+
+        //Ejemplo 2 - Números reales
+        System.out.println("Digite una nota (Real)...");
+        double nota = leer.nextDouble();
+        System.out.println("EL valor digitado es: " + nota);
+
+        leer.close();
+    }
+}
